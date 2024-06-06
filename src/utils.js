3a4b91d5f5f7818fc231dde309b6c7d786baf645
wendymasese <wendy.masese@student.moringaschool.com>
2024-06-06 18:39:18 +0300

    complete

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,35 @@
 // Your code here
+export function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+      if (["a", "e", "i", "o", "u"].includes(char)) {
+        points += 1;
+      } else {
+        points += 2;
+      }
+    }
+    return points;
+  }
+
+//   export function pointsForWord(word) {
+//     let points = 0;
+//     for (const char of word) {
+//       points += /[aeiou]/.test(char) ? 1 : 2;
+//     }
+//     return points;
+//   }
+// it("handles uppercase and lowercase input", () => {
+//     const word = "tEsT";
+  
+//     const points = pointsForWord(word);
+  
+//     expect(points).toBe(7);
+//   });
+//   export function pointsForWord(word) {
+//     let points = 0;
+//     for (const char of word) {
+//       points += /[aeiou]/i.test(char) ? 1 : 2;
+//     }
+//     return points;
+//   }
+
